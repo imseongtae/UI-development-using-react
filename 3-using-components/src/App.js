185deeb64bb156140353f7f83d0dc0e10854a8cf
imseongtae <dev.haemil@gmail.com>
2021-07-07 04:20:40 +0900

    feat: Add Header component

--- 3-using-components/src/App.js
+++ 3-using-components/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import Header from './components/Header'
+
 class App extends React.Component {
   render() {
-    return <>TODO: App 컴포넌트</>
+    return (
+      <>
+        <Header title="검색" />
+        TODO: App 컴포넌트
+      </>
+    )
   }
 };
 

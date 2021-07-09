7da328410939a1b378b381bc9ee0bd38f6fdd51b
imseongtae <dev.haemil@gmail.com>
2021-07-09 21:58:56 +0900

    feat: Update App to include SearchForm component

--- 3-using-components/src/App.js
+++ 3-using-components/src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
 import Header from './components/Header'
+import SearchForm from './components/SearchForm';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchKeyword: '',
+    }
+  }
+
+  handleChangeInput(value) {    
+    this.setState({ searchKeyword: value });
+  }  
+  search() {
+    // console.log();
+    console.log(this.state.searchKeyword);
+  }
+  handleReset() {
+    this.setState({ searchKeyword: '' })
+  }
+
   render() {
     return (
       <>
         <Header title="검색" />
+        <SearchForm
+          value={this.state.searchKeyword}
+          onChange={value => this.handleChangeInput(value)}
+          onSubmit={() => this.search()} 
+          onReset={() => this.handleReset()}
+        />
         TODO: App 컴포넌트
       </>
     )

9288c4887b388d42655479ba575dbcd039ff3054
imseongtae <dev.haemil@gmail.com>
2021-07-12 03:50:21 +0900

    refactor: Update App to display current search keyword

--- 4-using-cra/src/App.tsx
+++ 4-using-cra/src/App.tsx
@@ -16,11 +16,11 @@ const App:React.FunctionComponent = () => {
   const [searchResult, setSearchResult] = useState<SearchResultItem[] | []>([]);
   const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
   const [selectedTab, setSelectedTab] = useState<string>(TabType.KEYWORD);
-  
 
   const search = (keyword: string) => {
     console.log(keyword);
     const searchResult: SearchResultItem[] | [] = store.search(keyword);
+    setSearchKeword(keyword);
     setIsSubmitted(true);
     setSearchResult(searchResult);
   };
@@ -47,11 +47,11 @@ const App:React.FunctionComponent = () => {
         onReset={handleReset}
         onChange={(searchKeyword: string) => handleChangeInput(searchKeyword)}
       />
-      {/* TODO */}
       <Tabs selectedTab={selectedTab} onChange={selectedTab => setSelectedTab(selectedTab)} />
+      {/* 추천 및 최근 검색어 목록 */}
       {selectedTab === TabType.KEYWORD && <KeywordList onClick={searchKeyword => search(searchKeyword)} />}
       {selectedTab === TabType.HISTORY && <HistoryList onClick={searchKeyword => search(searchKeyword)} />}
-
+      {/* 검색 결과 목록 */}
       {isSubmitted && <SearchResult data={searchResult} />}
     </>
   );

0bf7901501c2fdec77efb6012d228fbbd2d91c2f
imseongtae <dev.haemil@gmail.com>
2021-07-11 22:53:11 +0900

    feat: Update App to include Header and SearchForm

--- 4-using-cra/src/App.tsx
+++ 4-using-cra/src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './style.css';
+import Header from 'components/Header';
+import SearchForm from 'components/SearchForm';
+
+
+import store from 'data/Store';
+
+const data = store.getHistoryList();
+console.log(data);
 
 function App() {
+  const [keyword, setKeword] = useState<string>('');  
+
+  const search = (keyword: string) => {
+    console.log(keyword);
+  };
+
+  const handleReset = () => {
+    console.log('reset');    
+  };  
+
+  // const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {    
+  //   setKeword(event.target.value);
+  // }
+  const handleChangeInput = (keyword: string) => {    
+    setKeword(keyword);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="검색" />
+      <SearchForm 
+        value={keyword}
+        onSubmit={() => search(keyword)}
+        onReset={handleReset}
+        onChange={(keyword: string) => handleChangeInput(keyword)}
+      />
+      {/* TODO */}
+    </>
   );
 }
 

91d6fa7006979b44d8d967d87dd837c4c065e71d
imseongtae <dev.haemil@gmail.com>
2021-07-12 01:38:58 +0900

    feat: Add SearchResult component

---
+++ 4-using-cra/src/components/SearchResult.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export interface SearchResultItem {
+  id: number; name: string; imageUrl: string;
+};
+
+export interface SearchResultItems {  
+  // data: {id: number; name: string; imageUrl: string}
+  data: Array<SearchResultItem>
+};
+
+const SearchResult: React.FunctionComponent<SearchResultItems> = ({ data = [] }) => {
+  if (data.length <= 0) return <div className="empty-box">검색 결과가 없습니다.</div>
+
+  return (
+    <ul>
+      {data.map(item => (
+        <li key={item.id}>
+          <img src={item.imageUrl} alt={item.name} />
+          <p>{item.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+
+export default SearchResult;

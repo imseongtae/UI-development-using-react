26d043da4e4a81c718a0a695be969b00c274f9cf
imseongtae <dev.haemil@gmail.com>
2021-07-13 05:57:40 +0900

    feat: Update App to include SearchForm component

--- 5-webpack-with-ts/src/App.tsx
+++ 5-webpack-with-ts/src/App.tsx
@@ -1,14 +1,39 @@
 import React, { useState } from 'react';
 import './styles/main.css';
 import Header from './components/Header';
+import SearchForm from './components/SearchForm';
 import Tabs, { TabType } from './components/Tabs';
 
 const App: React.FunctionComponent = () => {
+  const [searchKeyword, setSearchKeyword] = useState<string>('');
   const [selectedTab, setSelectedTab] = useState<string>(TabType.KEYWORD);
+  // const [searchResult, setSearchResult] = useState<SearchResultItem[] | []>([]);
+  // const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+
+  const search = (searchKeyword: string): void => {
+    console.log(searchKeyword);
+    setSearchKeyword(searchKeyword);
+  };
+
+  const handleReset = (): void => {
+    console.log('reset');
+    setSearchKeyword('');
+    // setIsSubmitted(false);
+  };
+
+  const handleChangeInput = (searchKeyword: string): void => {
+    setSearchKeyword(searchKeyword);
+  };
 
   return (
     <>
       <Header title="검색" />
+      <SearchForm
+        value={searchKeyword}
+        onSubmit={() => search(searchKeyword)}
+        onReset={handleReset}
+        onChange={searchKeyword => handleChangeInput(searchKeyword)}
+      />
       <Tabs
         selectedTab={selectedTab}
         onChange={selectedTab => setSelectedTab(selectedTab)}
---
+++ 5-webpack-with-ts/src/components/SearchForm.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+interface Props {
+  value: string;
+  onSubmit: () => void;
+  onReset: () => void;
+  // onChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  onChange: (keyword: string) => void;
+}
+
+const SearchForm: React.FunctionComponent<Props> = ({
+  value,
+  onSubmit,
+  onReset,
+  onChange,
+}) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    onSubmit();
+  };
+
+  const handleReset = (): void => {
+    onReset();
+  };
+
+  // event 객체 자체를 넘길 수도 있음, 받는 쪽에서 event.target.value에 접근해야 함
+  // const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   onChange(event);
+  // }
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    const newValue: string = event.target.value;
+    onChange(newValue);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} onReset={handleReset}>
+      <input
+        type="text"
+        placeholder="검색어를 입력하세요"
+        value={value}
+        onChange={handleInputChange}
+      />
+      {value.length > 0 && <button type="reset" className="btn-reset" />}
+    </form>
+  );
+};
+
+export default SearchForm;

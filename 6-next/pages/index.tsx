bf63ca2115a83d94eda0e9e5eb0c64dd5b2593e8
imseongtae <dev.haemil@gmail.com>
2021-07-16 03:10:59 +0900

    feat: Update index page to include SearchResultList and Tabs

--- 6-next/pages/index.tsx
+++ 6-next/pages/index.tsx
@@ -4,18 +4,30 @@ import Image from 'next/image'
 
 import Header from '@/components/Header'
 import SearchForm from '~/components/SearchForm'
+import SearchResultList, { SearchResultItem } from '@/components/SearchResultList'
+import Tabs, { TabType } from '~/components/Tabs'
+
+
+import store from '~/data/Store';
 
 
 export default function Home() {
-  const [searchKeyword, setSearchKeyword] = useState<string>('');   
+  const [searchKeyword, setSearchKeyword] = useState<string>('');
+  const [searchResult, setSearchResult] = useState<SearchResultItem[] | []>([]);
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+  const [selectedTab, setSelectedTab] = useState<string>(TabType.KEYWORD);
 
   const search = () => {
     console.log(searchKeyword);
     setSearchKeyword(searchKeyword);
+    const searchResult = store.search(searchKeyword);
+    setIsSubmitted(true);
+    setSearchResult(searchResult);
   };
 
   const handleReset = () => {
     console.log('reset');
+    setIsSubmitted(false);
     setSearchKeyword('');
   };
 
@@ -32,6 +44,9 @@ export default function Home() {
         onReset={handleReset} 
         onChange={(searchKeyword) => handleInputChange(searchKeyword)}
       />
+      <Tabs selectedTab={selectedTab} onChange={(selectedTab) => setSelectedTab(selectedTab)} />
+      {isSubmitted && <SearchResultList data={searchResult} />}
+      
     </>
   );
 };

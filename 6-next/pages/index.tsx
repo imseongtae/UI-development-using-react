edee7a995b52b0f48ce10c7fbf8e2b1dd9d816df
imseongtae <dev.haemil@gmail.com>
2021-07-15 17:15:25 +0900

    feat: Update index page to include SearchForm component

--- 6-next/pages/index.tsx
+++ 6-next/pages/index.tsx
@@ -1,13 +1,37 @@
+import React, { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
-import Header from '../components/Header'
+
+import Header from '@/components/Header'
+import SearchForm from '~/components/SearchForm'
 
 
 export default function Home() {
+  const [searchKeyword, setSearchKeyword] = useState<string>('');   
+
+  const search = () => {
+    console.log(searchKeyword);
+    setSearchKeyword(searchKeyword);
+  };
+
+  const handleReset = () => {
+    console.log('reset');
+    setSearchKeyword('');
+  };
+
+  const handleInputChange = (searchKeyword: string) => {
+    setSearchKeyword(searchKeyword);
+  };
+
   return (
     <>
       <Header title="검색" />
-
+      <SearchForm
+        value={searchKeyword} 
+        onSubmit={() => search()} 
+        onReset={handleReset} 
+        onChange={(searchKeyword) => handleInputChange(searchKeyword)}
+      />
     </>
-  )
-}
+  );
+};

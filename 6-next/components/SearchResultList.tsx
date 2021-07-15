4a94a528c57ceaa99657a8b97d252d99c9035d0e
imseongtae <dev.haemil@gmail.com>
2021-07-16 03:10:03 +0900

    feat: Add SearchResultList component

---
+++ 6-next/components/SearchResultList.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export interface SearchResultItem {
+  id: number;
+  name: string;
+  imageUrl: string;
+};
+
+export interface SearchResultItems {
+  data: Array<SearchResultItem> | [];
+};
+
+const SearchResultList: React.FunctionComponent<SearchResultItems> = ({ data = [] }) => {
+  if (!data.length) return <div className="empty-box">검색 결과가 없습니다.</div>;
+
+  return (    
+    <ul>
+      {data.map(item => (
+        <li key={item.id}>
+          <img src={item.imageUrl} alt={item.name} />
+          <p>{item.name}</p>
+        </li>
+      ))}    
+    </ul>
+  );
+};
+
+export default SearchResultList;
